a8fc279dfc565cfa8a12d1449a0faff1ee553e72
Kayla <niu@email.arizona.edu>
2017-02-05 13:48:18 -0600

    Easy / Merge sorted Arrays

--- mergeArrays.java
+++ mergeArrays.java
@@ -2,11 +2,15 @@ import java.util.*;
 public class mergeArrays{
     public static int[] mergeArrays(int[] a1, int[] a2) {
         
+	
         int[] mergedArray = new int[a1.length + a2.length];
         int i1 = 0; 
         int i2 = 0; 
         int n  = 0; 
-        
+
+        // This solution has O(n) time complexity
+	// The shorter array will exit the while loop first 
+
         while (i1 < a1.length && i2 < a2.length)  {
          
             if (a1[i1] > a2[i2]) { 
@@ -22,7 +26,7 @@ public class mergeArrays{
             } 
         }
 
-        
+        // Need to grab the remaining values 
         while ( i1 < a1.length ) {
             
             mergedArray[n] = a1[i1];
@@ -43,10 +47,10 @@ public class mergeArrays{
         
     }
     public static void main(String[] args) {
-         int[] myArray     = new int[]{3, 4, 6, 10, 11, 15};
-		 int[] alicesArray = new int[]{1, 5, 8, 12, 14, 19};
-		 int[] mergedArray = new int[myArray.length + alicesArray.length];
-		 mergedArray =  mergeArrays(myArray, alicesArray);
+         	 int[] a1 = new int[]{3, 4, 6, 10, 11, 15};
+		 int[] a2 = new int[]{1, 5, 8, 12, 14, 19};
+		 int[] mergedArray = new int[a1.length+ a2.length];
+		 mergedArray =  mergeArrays (a1, a2);
         
          for (int i = 0; i < mergedArray.length; i++) {
          		
